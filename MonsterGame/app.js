ea02703995a87c6251deb9e435cd2eb3f1bf1eb4
Iktisad Rashid <iktisad.rashid@hotmail.com>
2021-04-05 14:43:37 +0600

    Almost Done

--- MonsterGame/app.js
+++ MonsterGame/app.js
@@ -1,5 +1,75 @@
-const a = 10;
-const b = 10;
-const c = a+b;
+new Vue({
+    el:'#app',
+    data: {
+        playerHealth:100,
+        monsterHealth:100,
+        gameIsRunning: false,
 
-c
+    },
+    methods: {
+        startGame:function(){
+            this.gameIsRunning = true;
+            this.playerHealth =100;
+            this.monsterHealth = 100;
+        },
+        attack:function(){
+            
+            this.monsterHealth -= this.calculateDamage(3, 10);
+            
+            if (this.checkWin()) {
+                return;
+            }
+            
+            this.monsterAttacks();  
+
+
+        },
+        specialAttack:function(){
+            this.monsterHealth -= this.calculateDamage(10, 20);
+            
+            if (this.checkWin()) {
+                return;
+            }
+            this.monsterAttacks();
+        },
+        monsterAttacks:function(){
+            this.playerHealth -= this.calculateDamage(5, 12);
+            this.checkWin();
+        },
+        heal:function(){
+            if (this.playerHealth<=90) {
+                
+                this.playerHealth +=10;
+                this.monsterAttacks();
+            }else{
+                this.playerHealth = 100;
+            }
+        },
+        giveUp:function(){
+            this.gameIsRunning = false;
+        },
+        
+        calculateDamage:function(min, max){
+           return Math.max(Math.floor(Math.random()*max)+1, min);
+        },
+        checkWin:function(){
+            
+            if (this.monsterHealth <= 0) {
+                if (confirm("You Won! New Game")) {
+                    this.startGame();
+                }else{
+                    this.gameIsRunning = false;
+                }    
+                return true;
+            }else if(this.playerHealth<=0){
+                if (confirm("You Lost! New Game")) {
+                    this.startGame();
+                }else{
+                    this.gameIsRunning = false;
+                }
+                return true;
+            }
+            return false;
+        },
+    },
+})
