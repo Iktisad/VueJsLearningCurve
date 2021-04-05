0ccc79cdcfc704303b8f60572448b84919a0a040
Iktisad Rashid <iktisad.rashid@hotmail.com>
2021-04-05 15:21:30 +0600

    Monster Game Project Done

--- MonsterGame/app.js
+++ MonsterGame/app.js
@@ -1,75 +1,87 @@
 new Vue({
-    el:'#app',
+    el: '#app',
     data: {
-        playerHealth:100,
-        monsterHealth:100,
+        playerHealth: 100,
+        monsterHealth: 100,
         gameIsRunning: false,
-
+        turns: []
     },
     methods: {
-        startGame:function(){
+        startGame: function () {
             this.gameIsRunning = true;
-            this.playerHealth =100;
+            this.playerHealth = 100;
             this.monsterHealth = 100;
+            this.turns = [];
         },
-        attack:function(){
-            
-            this.monsterHealth -= this.calculateDamage(3, 10);
-            
+        attack: function () {
+            var damage = this.calculateDamage(3, 10);
+            this.monsterHealth -= damage;
+            this.turns.unshift({
+                isPlayer: true,
+                text: 'Player hits Monster for ' + damage
+            });
             if (this.checkWin()) {
                 return;
             }
-            
-            this.monsterAttacks();  
 
-
+            this.monsterAttacks();
         },
-        specialAttack:function(){
-            this.monsterHealth -= this.calculateDamage(10, 20);
-            
+        specialAttack: function () {
+            var damage = this.calculateDamage(10, 20);
+            this.monsterHealth -= damage;
+            this.turns.unshift({
+                isPlayer: true,
+                text: 'Player hits Monster hard for ' + damage
+            });
             if (this.checkWin()) {
                 return;
             }
             this.monsterAttacks();
         },
-        monsterAttacks:function(){
-            this.playerHealth -= this.calculateDamage(5, 12);
-            this.checkWin();
-        },
-        heal:function(){
-            if (this.playerHealth<=90) {
-                
-                this.playerHealth +=10;
-                this.monsterAttacks();
-            }else{
+        heal: function () {
+            if (this.playerHealth <= 90) {
+                this.playerHealth += 10;
+            } else {
                 this.playerHealth = 100;
             }
+            this.turns.unshift({
+                isPlayer: true,
+                text: 'Player heals for 10'
+            });
+            this.monsterAttacks();
         },
-        giveUp:function(){
+        giveUp: function () {
             this.gameIsRunning = false;
         },
-        
-        calculateDamage:function(min, max){
-           return Math.max(Math.floor(Math.random()*max)+1, min);
+        monsterAttacks: function() {
+            var damage = this.calculateDamage(5, 12);
+            this.playerHealth -= damage;
+            this.checkWin();
+            this.turns.unshift({
+                isPlayer: false,
+                text: 'Monster hits Player for ' + damage
+            });
         },
-        checkWin:function(){
-            
+        calculateDamage: function(min, max) {
+            return Math.max(Math.floor(Math.random() * max) + 1, min);
+        },
+        checkWin: function() {
             if (this.monsterHealth <= 0) {
-                if (confirm("You Won! New Game")) {
+                if (confirm('You won! New Game?')) {
                     this.startGame();
-                }else{
+                } else {
                     this.gameIsRunning = false;
-                }    
+                }
                 return true;
-            }else if(this.playerHealth<=0){
-                if (confirm("You Lost! New Game")) {
+            } else if (this.playerHealth <= 0) {
+                if (confirm('You lost! New Game?')) {
                     this.startGame();
-                }else{
+                } else {
                     this.gameIsRunning = false;
                 }
                 return true;
             }
             return false;
-        },
-    },
-})
+        }
+    }
+});
